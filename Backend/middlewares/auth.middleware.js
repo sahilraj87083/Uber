069e94f2612fffe705dc785b59256230ada1af7d
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-28 18:56:38 +0530

    small debugging code removed

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -8,7 +8,7 @@ const verifyJWT = asyncHandler(async(req,res,next) => {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
     
         if(!token){
-            throw new ApiError(401, "Unauthorized request stage 1")
+            throw new ApiError(401, "Unauthorized request")
         }
     
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
