69a8e7d46c750e993506ca4b92f4b2cb9f78fbf7
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-28 16:38:09 +0530

    feat(auth): add user registration controller with validation and error handling

---
+++ Backend/controllers/user.controller.js
@@ -0,0 +1,62 @@
+import { User } from "../models/user.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import {validationResult} from 'express-validator'
+import { createUser } from "../services/user.services.js";
+import { ApiError } from "../utils/ApiError.js";
+import {ApiResponse} from '../utils/ApiResponse.js'
+
+const registerUser = asyncHandler(async (req, res, next) => {
+
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        throw new ApiError(400,"Validation failed", errors.array() )
+    }
+    // console.log(req.body)
+
+
+    const {fullName, email, password, username, contact} = req.body
+
+    // check if user pre existes
+    const existedUser = await User.findOne({
+        $or : [{email}, {username}]
+    })
+
+    if(existedUser){
+        throw new ApiError(409, "User with email or username already exists")
+    }
+
+    const user = await createUser({
+        firstName : fullName.firstName,
+        lastName : fullName.lastName,
+        username : username,
+        email : email,
+        password : password,
+        contact : contact
+
+    })
+
+    const createdUser = await User.findById(user?._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
+
+    return res.status(201)
+    .json(new ApiResponse(201, "User created Successfully", createdUser))
+
+    // const accessToken = createdUser.generateAccessToken();
+    // return res.status(201)
+    // .json(new ApiResponse(201, "User created Successfully", {
+    //     user : createdUser,
+    //     accessToken : accessToken
+    // }))
+
+})
+
+
+export {
+    registerUser
+}
