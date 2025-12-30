dd91efc337792e931420d7a71b59739452dbc487
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-30 20:08:56 +0530

    CORS setup done

--- Backend/app.js
+++ Backend/app.js
@@ -4,12 +4,20 @@ import { LIMIT } from './constants.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
-app.use(cors(
-    {
-        origin : process.env.CORS_ORIGIN,
-        credentials : true
-    }
-))
+// app.use(cors(
+//     {
+//         origin : process.env.CORS_ORIGIN,
+//         credentials : true
+//     }
+// ))
+// import cors from "cors";
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
+app.options(/.*/, cors());
 
 app.use(express.json({limit : LIMIT}))
 app.use(cookieParser())
