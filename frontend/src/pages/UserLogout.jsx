b27476e41df534a95ff7a6201b3d02accffa6c92
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-30 21:53:03 +0530

    feat(auth): implement user logout with backend API and state cleanup

---
+++ frontend/src/pages/UserLogout.jsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import {useUserContext} from '../contexts/UserContext'
+
+
+export const UserLogout = () => {
+    const navigate = useNavigate();
+    const {user, setUser, authToken, setAuthToken } = useUserContext()
+
+   useEffect(() => {
+
+    const logout = async () => {
+        try {
+            const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/api/v1/users/logout` , 
+                {
+                    headers : {
+                        Authorization: `Bearer ${authToken}`
+                    }
+                },
+                {
+                    withCredentials : true // ✅ send refresh token cookie
+                }
+            )
+        }
+        catch (error) {
+            console.error("Logout error:", error);
+        }
+        finally {
+            // ✅ clear frontend auth state no matter what
+            setAuthToken(null)
+            setUser(null)
+            navigate("/user/login");
+        }
+    };
+
+    logout();
+   }, [navigate, setUser, setAuthToken]);
+
+};
+
+export default UserLogout;
