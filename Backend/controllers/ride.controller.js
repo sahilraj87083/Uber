10c3eadd25620d2b4032668fbcbf6cf01c26dd7f
sahilraj87083 <sahilraj87093@gmail.com>
2026-01-06 23:11:18 +0530

    feat(rides): implement real-time ride lifecycle with socket-based matching

--- Backend/controllers/ride.controller.js
+++ Backend/controllers/ride.controller.js
@@ -5,15 +5,109 @@ import {
     confirmRideService,
     startRideService,
     endRideService } from "../services/rides.services.js";
-
+import {getCaptainsInTheRadiusService, getAddressCoordinateService} from '../services/maps.services.js'
 import { Ride } from "../models/ride.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { validationResult } from "express-validator";
+import {sendMessageToSocketId} from '../socket.js'
+import { isValidObjectId } from "mongoose";
 
-const createRide = asyncHandler(async () => {
+const createRide = asyncHandler(async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(new ApiError(400, 'Error while creating ride', {message : "error in the input",errors: errors.array()}));
+        }
+        const { pickup, destination, vehicleType } = req.body;
+        const userId = req.user._id;
+    
+        // Create ride 
+        const ride = await createRideService({
+            user: userId,
+            pickup,
+            destination,
+            vehicleType,
+        });
+    
+        if(!ride) {
+            return res.status(500).json(new ApiError(500, 'Error while creating ride', {}))
+        }
 
+        // RESPOND IMMEDIATELY 
+        ride.otp = ""
+    
+        res.status(200).json(
+            new ApiResponse(200, "Ride created", { ride })
+        );
+    
+        // BACKGROUND WORK (non-blocking)
+    
+        (async () => {
+            try {
+                const pickupCoordinates = await getAddressCoordinateService(pickup)
+        
+                // console.log(pickupCoordinates)
+        
+                const captainsInRadius = await getCaptainsInTheRadiusService(pickupCoordinates.lat, pickupCoordinates.lng, 2)
+                // console.log("Nearby captains:", captainsInRadius);
+                const rideWithUser = await Ride.aggregate([
+                    {
+                        $match : { _id : ride._id}
+                    },
+                    {
+                        $lookup : {
+                            from : 'users',
+                            localField : 'user',
+                            foreignField : '_id',
+                            as : 'user',
+                            pipeline : [
+                                {
+                                    $project : {
+                                        fullName : 1,
+                                        contact : 1,
+                                        email : 1,
+                                        socketId : 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $unwind : '$user'
+                    },
+                    {
+                        $project : {
+                            pickup : 1,
+                            destination : 1,
+                            fare : 1,
+                            status : 1,
+                            otp : 1,
+                            distance : 1,
+                            duration : 1,
+                            createdAt : 1,
+                            user : 1
+                        }
+                    }
+                ])
+
+
+
+                captainsInRadius.map((captain) => {
+                    sendMessageToSocketId(captain.socketId, {
+                        event : 'new-ride-request',
+                        data : rideWithUser[0]
+                    })
+                })
+        
+            } catch (error) {
+                console.log("Background ride-matching error:", error.message);
+            }
+        })();
+    } catch (error) {
+        return res.status(500).json(new ApiError(500, 'Server Error While creating ride', {message: error.message }));
+    }
 })
 
 const  getFare = asyncHandler(async (req,res) => {
@@ -35,17 +129,91 @@ const  getFare = asyncHandler(async (req,res) => {
 
 })
 
-const confirmRide = asyncHandler(async () => {
+const confirmRide = asyncHandler(async (req, res) => {
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(new ApiError(400, 'Error while confirming the ride', {errors: errors.array()}));
+    }
 
+    const {rideId} = req.body
+    try {
+        
+        const ride = await confirmRideService({ rideId, captain: req.captain });
+
+        if(!ride){
+            return res.status(404).json(new ApiError(404, 'Ride not found or already confirmed', {}));
+        }
+
+        sendMessageToSocketId(ride.user.socketId, {
+            event : 'ride-confirmed',
+            data : ride
+        })
+
+        
+        
+        return res.status(200).json(new ApiResponse(200, 'Ride confirmed successfully', { ride }));
+
+    } catch (error) {
+        return res.status(500).json(new ApiError(500, 'Server Error While confirming the ride', {message: error.message }));
+    }
 })
 
 
-const startRide = asyncHandler(async () => {
+const startRide = asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(new ApiError(400, 'Error while starting the ride', {errors: errors.array()}));
+    }
 
+    const {rideId , otp} = req.query
+
+    if(!rideId || !otp ){
+        throw new ApiError(400, 'RideId and Opt are required', {});
+    }
+    if(!isValidObjectId(rideId)){
+        throw new ApiError(400, 'Invalid RideId', {});
+    }
+
+    try {
+        const ride = await startRideService({rideId, otp, captain : req.captain})
+        // console.log("in the ride controller" , ride)
+
+        sendMessageToSocketId(ride.user.socketId,
+            {
+                event : 'ride-started',
+                data : ride
+            }
+        )
+
+        return res.status(200).json(new ApiResponse(200, 'Ride started!! Happy Journey', {ride : ride}))
+
+    } catch (error) {
+        return res.status(500).json(new ApiError(500, 'Server Error while starting the ride', {message: error.message }));
+    }
 })
 
-const endRide = asyncHandler(async () => {
+const endRide = asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(new ApiError(400, 'Error while ending the ride', {errors: errors.array()}));
+    }
 
+    const {rideId} = req.body
+
+    try {
+        const ride = await endRideService({ rideId, captain: req.captain });
+
+        sendMessageToSocketId(ride.user.socketId, {
+            event : 'ride-ended',
+            data : ride
+        })
+
+        return res.status(200).json(new ApiResponse(200, 'Ride Ended' , {ride : ride}))
+
+    } catch (error) {
+        return res.status(500).json(new ApiError(500, 'Server Error while ending the ride', {message: error.message }));
+    }
 })
 
 
--- Backend/services/rides.services.js
+++ Backend/services/rides.services.js
@@ -1,5 +1,9 @@
 import { getDistanceTimeService } from "../services/maps.services.js"
 import { Ride } from "../models/ride.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import crypto from 'crypto'
+import mongoose from "mongoose"
+
 
 const getFareService = async(pickup, destination) => {
     if(!pickup || !destination){
@@ -48,29 +52,282 @@ const createRideService = async ({user, pickup, destination, vehicleType}) => {
         throw new ApiError(400, 'All fields are required')
     }
 
-    const fare = await getFare(pickup, destination);
+    const fare = await getFareService(pickup, destination);
 
     const ride = await Ride.create({
         user,
         pickup,
         destination,
-        otp : getOtp(6),
+        otp : getOtpService(6),
         fare : fare[vehicleType]
     })
 
     return ride
 }
 
-const confirmRideService = async() => {
+const confirmRideService = async( {rideId, captain}) => {
+    if(!rideId){
+        throw new ApiError(400, 'RideId is required')
+    }
 
+    await Ride.findOneAndUpdate(
+        {
+            _id : rideId
+        },
+        {
+            status : 'accepted',
+            captain : captain._id
+        }
+    )
+
+    const ride = await Ride.aggregate([
+        {
+            $match : { _id : new mongoose.Types.ObjectId(rideId) }
+        },
+        {
+            $lookup : {
+                from : 'users',
+                localField : 'user',
+                foreignField : '_id',
+                as : 'user',
+                pipeline : [
+                    {
+                        $project : {
+                            fullName : 1,
+                            contact : 1,
+                            email : 1,
+                            socketId : 1
+                        }
+                    }  
+                ]
+            }
+        },{
+            $unwind : '$user'
+        },
+        {
+            $lookup : {
+                from : 'captains',
+                localField : 'captain',
+                foreignField : '_id',
+                as : 'captain',
+                pipeline : [
+                    {
+                        $project : {
+                            fullName : 1,
+                            contact : 1,
+                            email : 1,
+                            socketId : 1,
+                            vehicle : 1,
+                            location : 1
+                        }
+                    }  
+                ]
+            }
+        },
+        {
+            $unwind : '$captain'
+        },{
+            $project : {
+                pickup : 1,
+                destination : 1,
+                fare : 1,
+                status : 1,
+                otp : 1,
+                distance : 1,
+                duration : 1,
+                createdAt : 1,
+                user : 1,
+                captain : 1
+            }
+        }
+    ])
+
+    // console.log(ride)
+
+    if(!ride || ride.length === 0){
+        throw new ApiError(404, 'Ride not found')
+    }
+    
+    return ride[0]
 }
 
-const startRideService = async() => {
+const startRideService = async({rideId, otp, captain}) => {
+    if(!rideId || !otp){
+        throw new ApiError(400, 'RideId and Opt are required', {});
+    }
 
+    const ride = await Ride.aggregate([
+        {
+            $match : {
+                _id : new mongoose.Types.ObjectId(rideId)
+            }
+        },
+        {
+            $lookup : {
+                from : 'users',
+                localField : 'user',
+                foreignField : '_id',
+                as : 'user',
+                pipeline : [
+                    {
+                        $project : {
+                            fullName : 1,
+                            contact : 1,
+                            email : 1,
+                            socketId : 1
+                        }
+                    }  
+                ]
+            }
+        },{
+            $unwind : '$user'
+        },
+        {
+            $lookup : {
+                from : 'captains',
+                localField : 'captain',
+                foreignField : '_id',
+                as : 'captain',
+                pipeline : [
+                    {
+                        $project : {
+                            fullName : 1,
+                            contact : 1,
+                            email : 1,
+                            socketId : 1,
+                            vehicle : 1,
+                            location : 1
+                        }
+                    }  
+                ]
+            }
+        },
+        {
+            $unwind : '$captain'
+        },
+        {
+            $project : {
+                pickup : 1,
+                destination : 1,
+                fare : 1,
+                status : 1,
+                otp : 1,
+                distance : 1,
+                duration : 1,
+                createdAt : 1,
+                user : 1,
+                captain : 1
+            }
+        }
+    ])
+
+    if(!ride || ride.length === 0){
+         throw new ApiError(404, 'Ride not found')
+    }
+
+    if(ride[0].status !== 'accepted'){
+         throw new ApiError(404, 'Ride not accepted')
+    }
+
+    if(ride[0].otp !== otp){
+         throw new ApiError(400, 'Invalid Otp')
+    }
+
+    await Ride.findByIdAndUpdate(rideId, 
+        {
+            status : 'ongoing'
+        }
+    )
+
+    return ride[0];
 }
 
-const endRideService = async() => {
+const endRideService = async({rideId , captain}) => {
+    if(!rideId){
+        throw new ApiError(400, 'rideId is required') 
+    }
 
+    const ride = await Ride.aggregate([
+        {
+            $match : {
+                _id : new mongoose.Types.ObjectId(rideId),
+                captain : captain._id
+            }
+        },
+        {
+            $lookup : {
+                from : 'captains',
+                localField : 'captain',
+                foreignField : '_id',
+                as : 'captain',
+                pipeline : [
+                    {
+                        $project : {
+                            fullName : 1,
+                            contact : 1,
+                            email : 1,
+                            socketId : 1,
+                            vehicle : 1,
+                            location : 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $unwind : '$captain'
+        },
+        {
+            $lookup : {
+                from : 'users',
+                localField : 'user',
+                foreignField : '_id',
+                as : 'user',
+                pipeline : [
+                    {
+                        $project : {
+                            fullName : 1,
+                            contact : 1,
+                            email : 1,
+                            socketId : 1
+                        }
+                    }  
+                ]
+            }
+        },{
+            $unwind : '$user'
+        },
+        {
+            $project : {
+                pickup : 1,
+                destination : 1,
+                fare : 1,
+                status : 1,
+                otp : 1,
+                distance : 1,
+                duration : 1,
+                createdAt : 1,
+                user : 1,
+                captain : 1
+            }
+        }
+    ])
+
+    if(!ride || ride.length === 0){
+        throw new ApiError(404, 'Ride not found')
+    }
+
+    if(ride[0].status !== 'ongoing'){
+        throw new ApiError(400, 'Ride is not ongoing')
+    }
+
+    await Ride.findByIdAndUpdate(rideId, 
+        {
+            status : 'completed'
+        }
+    )
+
+    return ride[0];
 }
 
 
